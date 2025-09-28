6f37ebcaaea82b5e1b662690d2532780c4498f16
Filipe Nery <fineryrabelo@gmail.com>
2025-09-27 22:34:21 -0300

    feat: add home screen and initial project setup

---
+++ code/GameSpace-Back/src/main/java/com/estudo/gamespace/GameSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.gamespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameSpaceApplication.class, args);
+	}
+
+}
